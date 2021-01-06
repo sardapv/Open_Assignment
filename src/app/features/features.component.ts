c6afed1a1666d7147fd2ddea6b3d8ca4117f9494
Pranav Sarda <sardapranav1@gmail.com>
2021-01-06 21:13:52 +0530

    install cli-gh-pages :rocket:
    install cli-gh-pages :rocket:

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -14,27 +14,27 @@ export class FeaturesComponent {
   constructor() {
     this.cards.push(
       {
-        image: '../../assets/img/card/Get Paid Instantly.png',
+        image: 'assets/img/card/Get Paid Instantly.png',
         title: 'Quick cash disbursement',
         desc: 'Get terms loans that your business needs within 72 hrs'
       },
       {
-        image: '../../assets/img/card/Low interest rate.png',
+        image: 'assets/img/card/Low interest rate.png',
         title: 'Low-interest rate',
         desc: 'Achieve your financial goals with an amazing interest rate starting at 13% per annum'
       },
       {
-        image: '../../assets/img/card/Secure Payments.png',
+        image: 'assets/img/card/Secure Payments.png',
         title: 'Zero Paperwork',
         desc: 'Get started instantly by submitting only your basic details & bank statements'
       },
       {
-        image: '../../assets/img/card/freelancer_feature_icon.png',
+        image: 'assets/img/card/freelancer_feature_icon.png',
         title: 'Ace your business finances',
         desc: 'Manage banking, accounting & everything in between, on one platform'
       },
       {
-        image: '../../assets/img/card/Covid.png',
+        image: 'assets/img/card/Covid.png',
         title: 'Loans to fight COVID-19',
         desc: 'Zero EMI for first 3 months on Back-to-Business loans of upto 1 lakh'
       });
